0bc93bacf73fc78c06d18ed099f52a6ddcdf2e90
Yousaf Shibin <yousafmpshibin@gmail.com>
2025-08-08 16:59:36 +0530

    message

---
+++ largest.js
@@ -0,0 +1,11 @@
+function findLargest(arr) {
+  if (arr.length === 0) return undefined; 
+  let max = arr[0];           
+  for (let i = 1; i < arr.length; i++) { 
+    if (arr[i] > max) {       
+      max = arr[i];           
+    }
+  }
+  return max;
+}
+console.log(findLargest([2,72,22,5,79]))
