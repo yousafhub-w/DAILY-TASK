b63bf74830ed05979a347f67b45344efd34ec322
Yousaf Shibin <yousafmpshibin@gmail.com>
2025-08-12 12:21:09 +0530

    longest word

---
+++ longestword.js
@@ -0,0 +1,12 @@
+function b(n){
+    if(n%3==0 && n%5==0){
+        console.log("FizzBuzz");
+        
+    }else if (n%3==0){
+        console.log("Buzz");
+    }else{
+        console.log("Fizz");
+        
+    }
+}b(15)
+
