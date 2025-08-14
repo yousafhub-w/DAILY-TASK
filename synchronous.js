78810da456044931f262b21fd1e929d121230033
Yousaf Shibin <yousafmpshibin@gmail.com>
2025-08-14 16:26:01 +0530

    Synchronous

---
+++ synchronous.js
@@ -0,0 +1,42 @@
+function loadingImg(){
+    console.log("loadingImg Started...");
+    
+    let start= Date.now();
+    let delay= 3000;
+    let end =  start+delay;
+    console.log("loadingImg Executing...");
+    
+    while(Date.now() <= end){
+
+    }console.log("loadingImg Completed...");
+    
+}
+function loadingImgg(){
+    console.log("loadingImg Started...");
+    
+    let start= Date.now();
+    let delay= 4000;
+    let end =  start+delay;
+    console.log("loadingImg Executing...");
+    
+    while(Date.now() <= end){
+
+    }console.log("loadingImg Completed...");
+    
+}
+function loadingIm(){
+    console.log("loadingImg Started...");
+    
+    let start= Date.now();
+    let delay= 5000;
+    let end =  start+delay;
+    console.log("loadingImg Executing...");
+    
+    while(Date.now() <= end){
+
+    }console.log("loadingImg Completed...");
+    
+}
+loadingImg();
+loadingImgg();
+loadingIm();
