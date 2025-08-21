fa70e1040e9420ae715659907fe74e0bb60b6659
Yousaf Shibin <yousafmpshibin@gmail.com>
2025-08-21 10:54:57 +0530

    Fetching

---
+++ fetch.js
@@ -0,0 +1,18 @@
+function fetchData(){
+    return new Promise((resolve,reject)=>{
+        fetch('https://api.weather.gov/gridpoints/OKX/35,35/forecast')
+        .then(response=> response.json())
+        .then(data=> resolve(data.properties.periods[1].shortForecast));
+    })
+}
+function displayData(weather){
+    console.log(weather);
+}
+
+function onError(err){
+    console.log(`ERROR ${err}`);
+    
+}
+fetchData()
+ .then(displayData)
+ .catch(onError)
