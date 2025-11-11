b2e78f6f6e66cd52689684496d44bfb1e0d2f635
Yousaf Shibin <yousafmpshibin@gmail.com>
2025-11-11 23:59:39 +0530

    fibonacci

---
+++ fibonacci.js
@@ -0,0 +1,15 @@
+var fib = function(n) {
+    if (n < 2) return n;
+
+    let a = 0;
+    let b = 1;
+
+    for (let i = 2; i <= n; i++) {
+        let sum = a + b;
+        a = b;
+        b = sum;
+    }
+
+    return b;
+};
+console.log(fib(6));
